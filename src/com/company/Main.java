1bd133901f00b183802dde14e0e1e2fca65cca12
dp-ptcstd-29 <dp-ptcstd-29@ptc.local>
2019-10-09 14:00:05 +0300

    b2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("master 1");
+        System.out.println("master 1 - branch 1");
 
         System.out.println("b1");
 	// write your code here
