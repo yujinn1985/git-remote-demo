a7d5171a8d3057cdca895948abe599680120a88f
dp-ptcstd-29 <dp-ptcstd-29@ptc.local>
2019-10-09 13:58:09 +0300

    M2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("master 1");
+
+        System.out.println("master 2");
 	// write your code here
     }
 }
