fdd63825b61797f2447131ac01546d99675e42e4
dp-ptcstd-29 <dp-ptcstd-29@ptc.local>
2019-10-09 13:56:30 +0300

    init B1

---
+++ src/com/company/B1.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class B1 {
+
+    public static void main(String[] args) {
+        System.out.println("B1");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("master 1");
+
+        System.out.println("b1");
 	// write your code here
     }
 }
